406e3c9a9a88fcaed8386fe40be66250db568728
WorthyFire <worthyfire@vk.com>
2024-01-29 14:17:44 +0700

    practical work 6

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         description: "A pair of warm, fuzzy socks",
-        inStock: true,
+        inStock: false,
         inventory: 100,
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         onSale: true,
